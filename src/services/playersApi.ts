f1f834e58a9192916461217811d819dead587dfd
pausm99 <pausm99@gmail.com>
2024-03-18 15:50:31 +0100

    hided API url in environment variable

--- src/services/playersApi.ts
+++ src/services/playersApi.ts
@@ -1,12 +1,18 @@
 import { Player } from "../types/Player";
 
+const apiURL = import.meta.env.VITE_API_URL;
+
 export async function getPlayers(): Promise<Player[] | []> {
-    try {
-      const response = await fetch('http://localhost:3000/players');
-      return await response.json();
-    } catch (error) {
-      console.error('Error obtaining players', error);
-      return [];
+  try {
+    const response = await fetch(`${apiURL}/players`);
+    if (!response.ok) {
+        throw new Error(`Failed to get players. Status: ${response.status}`);
     }
+    return await response.json();
+  } catch (error) {
+      console.error('Error obtaining players', error);
+      throw error;
+  }
 }
+
   
