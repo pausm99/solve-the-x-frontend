295e4402ca498fe1c8c1fd7d79d33833712010d8
pausm99 <pausm99@gmail.com>
2024-03-18 22:05:46 +0100

    added register view to routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { RouteRecordRaw, createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import RegisterView from "../views/RegisterView.vue";
 
 const routes: RouteRecordRaw[] = [
     {
         path: '/',
         name: 'Home',
         component: HomeView
+    },
+    {
+        path: '/register',
+        name: 'Register',
+        component: RegisterView
     }
 ];
 
