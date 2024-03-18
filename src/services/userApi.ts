d67b923ba93b0be03a55eb54ed2a504440306eab
pausm99 <pausm99@gmail.com>
2024-03-18 22:36:02 +0100

    added API function call to login

--- src/services/userApi.ts
+++ src/services/userApi.ts
@@ -19,3 +19,23 @@ export async function register(username: string, password: string) {
       console.error('Error during register process:', error);
     }
 };
+
+export async function login(username: string, password: string) {
+  try {
+    const response = await fetch(`${apiUrlUsers}/login`, {
+      method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+    });
+
+    return response;
+    
+  } catch (error) {
+    console.error('Error during login process:', error);
+  }
+}
