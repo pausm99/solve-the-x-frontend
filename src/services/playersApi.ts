c20bba02b599022adfb6c69d7f8a6d7567f4cd6e
pausm99 <pausm99@gmail.com>
2024-03-18 16:22:57 +0100

    added api call to delete players + handled empty response on getPlayers function

--- src/services/playersApi.ts
+++ src/services/playersApi.ts
@@ -1,18 +1,36 @@
 import { Player } from "../types/Player";
 
-const apiURL = import.meta.env.VITE_API_URL;
+const apiUrlPlayers = import.meta.env.VITE_API_URL + '/players';
 
 export async function getPlayers(): Promise<Player[] | []> {
   try {
-    const response = await fetch(`${apiURL}/players`);
+    const response = await fetch(`${apiUrlPlayers}`);
+
     if (!response.ok) {
         throw new Error(`Failed to get players. Status: ${response.status}`);
     }
+
+    if (response.status === 204) return [];
+
     return await response.json();
   } catch (error) {
       console.error('Error obtaining players', error);
-      throw error;
+      return [];
   }
 }
 
   
+export async function deletePlayerById(id: number) {
+    try {
+      const response = await fetch(`${apiUrlPlayers}/${id}`, {
+        method: 'DELETE'
+      });
+      if (!response.ok) {
+        throw new Error(`Failed to delete player. Status: ${response.status}`);
+    }
+      return response.ok;
+    } catch (error) {
+      console.error('Error deleting player', error);
+      return false;
+    }
+}
