bc7555f24875ab265ab50ec87bd482e92918fe1a
pausm99 <pausm99@gmail.com>
2024-03-18 16:23:27 +0100

    added logic to delete player + styled view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,15 +8,19 @@
           <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Postion</th>
           <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Height (cm)</th>
           <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Weight (kg)</th>
+          <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Actions</th>
         </tr>
       </thead>
       <tbody class="bg-white divide-y divide-gray-200">
-        <tr v-for="(player, index) in players" :key="index">
+        <tr v-for="player in players" :key="player.id">
           <td class="px-6 py-4 whitespace-nowrap">{{ player.name }}</td>
           <td class="px-6 py-4 whitespace-nowrap">{{ player.age }}</td>
           <td class="px-6 py-4 whitespace-nowrap">{{ player.position }}</td>
           <td class="px-6 py-4 whitespace-nowrap">{{ player.height }}</td>
           <td class="px-6 py-4 whitespace-nowrap">{{ player.weight }}</td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <button @click="deletePlayer(player.id)" class="bg-red-700">DELETE</button>
+          </td>
         </tr>
       </tbody>
     </table>
@@ -26,36 +30,38 @@
 <script lang="ts">
 import { defineComponent, reactive, onMounted } from 'vue';
 import { Player } from '../types/Player';
-import { getPlayers } from '../services/playersApi';
+import { deletePlayerById, getPlayers } from '../services/playersApi';
 
 export default defineComponent({
   setup() {
     const players = reactive<Player[]>([]);
 
-    onMounted(async () => {
-      const fetchedPlayers = await getPlayers();
-      players.push(...fetchedPlayers);
-    });
+    const fetchPlayers = async () => {
+      try {
+        const fetchedPlayers = await getPlayers();
+        players.push(...fetchedPlayers);
+      } catch (error) {
+        console.log('Error fetching players', error);
+      }
+    };
 
-    return { players };
+    onMounted(fetchPlayers);
+
+    const deletePlayer = async (id: number) => {
+      try {
+        const response = await deletePlayerById(id);
+        if (response) {
+          const index = players.findIndex(player => player.id === id);
+          if (index !== -1) {
+            players.splice(index, 1);
+          }
+        }
+      } catch (error) {
+        console.log('Error deleting player', error);
+      }
+    };
+
+    return { players, deletePlayer };
   }
 });
 </script>
-
-<style scoped>
-  .player-table {
-    width: 100%;
-    border-collapse: collapse;
-  }
-
-  .player-table th,
-  .player-table td {
-    border: 1px solid #dddddd;
-    padding: 8px;
-    text-align: left;
-  }
-
-  .player-table th {
-    background-color: #f2f2f2;
-  }
-</style>
