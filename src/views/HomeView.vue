d612a5e93b19b94736c406e6c4cbcaaa93198eb8
pausm99 <pausm99@gmail.com>
2024-03-18 18:34:33 +0100

    created form and logic to create players + reorganized script

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <form @submit.prevent="addPlayer" novalidate>
     <table class="min-w-full divide-y divide-gray-200">
       <thead>
         <tr class="bg-gray-50">
@@ -19,35 +19,73 @@
           <td class="px-6 py-4 whitespace-nowrap">{{ player.height }}</td>
           <td class="px-6 py-4 whitespace-nowrap">{{ player.weight }}</td>
           <td class="px-6 py-4 whitespace-nowrap">
-            <button @click="deletePlayer(player.id)" class="bg-red-700">DELETE</button>
+            <button type="button" @click="deletePlayer(player.id!)" class="bg-red-700 w-full">DELETE</button>
+          </td>
+        </tr>
+        <tr>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <input v-model="playerData.name" type="text" minlength="1" maxlength="40" placeholder="Name" class="input-field" required>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <input v-model.number="playerData.age" type="number" min="1" max="100" placeholder="Age" class="input-field" required>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <input v-model="playerData.position" type="text" placeholder="Position" class="input-field" required>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <input v-model.number="playerData.height" type="number" min="100" max="300" placeholder="Height (cm)" class="input-field" required>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <input v-model.number="playerData.weight" type="number" min="20" max="200" placeholder="Weight (kg)" class="input-field" required>
+          </td>
+          <td class="px-6 py-4 whitespace-nowrap">
+            <button type="submit" class="bg-green-500 w-full">ADD</button>
           </td>
         </tr>
       </tbody>
     </table>
-  </div>
+    <div class="text-red-500 flex flex-col">
+      <span v-for="error in v$.$errors" :key="error.$uid">
+        {{ error.$property }} - {{ error.$message }}
+      </span>
+    </div>
+  </form>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, onMounted } from 'vue';
+import { defineComponent, reactive, onMounted, watch } from 'vue';
+import { required, minValue, maxValue, minLength, maxLength } from '@vuelidate/validators';
+import useVuelidate from '@vuelidate/core';
 import { Player } from '../types/Player';
-import { deletePlayerById, getPlayers } from '../services/playersApi';
+import { createPlayer, deletePlayerById, getPlayers } from '../services/playersApi';
 
 export default defineComponent({
   setup() {
+    // Reactive data
     const players = reactive<Player[]>([]);
+    const playerData = reactive<Player>({
+      name: '',
+      age: NaN,
+      position: '',
+      height: NaN,
+      weight: NaN,
+    });
 
-    const fetchPlayers = async () => {
+    // Fetch players on component mount
+    onMounted(fetchPlayers);
+
+    // Fetch players from API
+    async function fetchPlayers() {
       try {
         const fetchedPlayers = await getPlayers();
         players.push(...fetchedPlayers);
       } catch (error) {
-        console.log('Error fetching players', error);
+        console.error('Error fetching players', error);
       }
-    };
+    }
 
-    onMounted(fetchPlayers);
-
-    const deletePlayer = async (id: number) => {
+    // Delete player by ID
+    async function deletePlayer(id: number) {
       try {
         const response = await deletePlayerById(id);
         if (response) {
@@ -57,11 +95,54 @@ export default defineComponent({
           }
         }
       } catch (error) {
-        console.log('Error deleting player', error);
+        console.error('Error deleting player', error);
       }
+    }
+
+    // Add new player
+    async function addPlayer() {
+      const isValid = await v$.value.$validate();
+      if (isValid) {
+        try {
+          const createdPlayer = await createPlayer(playerData);
+          if (createdPlayer) {
+            players.push(createdPlayer);
+            clearForm();
+          } else {
+            console.error('Failed to create player');
+          }
+        } catch (error) {
+          console.error('Error adding player:', error);
+        }
+      }
+    }
+
+    // Clear form data and reset validation state
+    function clearForm() {
+      Object.assign(playerData, {
+        name: '',
+        age: NaN,
+        position: '',
+        height: NaN,
+        weight: NaN,
+      });
+      v$.value.$reset();
+    }
+
+    // Validation rules
+    const rules = {
+      name: { required, minLength: minLength(1), maxLength: maxLength(40) },
+      age: { required, minValue: minValue(1), maxValue: maxValue(100) },
+      position: { required },
+      height: { required, minValue: minValue(100), maxValue: maxValue(300) },
+      weight: { required, minValue: minValue(40), maxValue: maxValue(200) },
     };
 
-    return { players, deletePlayer };
+    // Vuelidate instance
+    const v$ = useVuelidate(rules, playerData);
+
+    return { players, deletePlayer, addPlayer, playerData, v$ };
   }
 });
+
 </script>
