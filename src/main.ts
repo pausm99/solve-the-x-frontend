8bab24d7e66ff4d929147a17394aeda4e4ca368d
pausm99 <pausm99@gmail.com>
2024-03-19 00:05:13 +0100

    added interceptors to app

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router';
+import { configureResponseInterceptor, configureRequestInterceptor } from './services/interceptors';
+
+
+configureResponseInterceptor();
+configureRequestInterceptor();
+
 
 createApp(App)
-    .use(router)    
-    .mount('#app');
+  .use(router)
+  .mount('#app');
