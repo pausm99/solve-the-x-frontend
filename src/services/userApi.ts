d6fa7b7bf1eafcaa6da169dda034608e69d14aed
pausm99 <pausm99@gmail.com>
2024-03-18 22:06:20 +0100

    added API function call to register user

---
+++ src/services/userApi.ts
@@ -0,0 +1,21 @@
+const apiUrlUsers = import.meta.env.VITE_API_URL + '/users';
+
+export async function register(username: string, password: string) {
+    try {
+      const response = await fetch(`${apiUrlUsers}/register`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          username: username,
+          password: password
+        })
+      });
+
+      return response;
+
+    } catch (error) {
+      console.error('Error during register process:', error);
+    }
+};
